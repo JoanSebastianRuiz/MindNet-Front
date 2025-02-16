8484916640d9757b5e384a806fdfff40292a59d0
JoanSebastianRuiz <joansebastianruizangarita10@gmail.com>
2025-02-15 23:45:05 -0500

    feat: :sparkles: Validators and UserContext

---
+++ src/context/UserContext.jsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { useContext, useState, createContext } from "react"
+
+const UserContext = createContext();
+
+export const UserContextProvider = ({children}) =>{
+    const [user, setUser] = useState(null);
+    return (
+        <UserContext.Provider value={{user, setUser}}>
+            {children}
+        </UserContext.Provider>
+    )
+}
+
+export const useUser = () => useContext(UserContext);
